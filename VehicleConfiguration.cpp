b39fb132ed975670ece3a424a2b0f6672901ae51
brendanglancy <bglance68@gmail.com>
2023-10-13 02:09:51 -0400

    improvement on repeated code

--- VehicleConfiguration.cpp
+++ VehicleConfiguration.cpp
@@ -10,6 +10,7 @@ std::string VehicleConfiguration::inputWithPrompt(const std::string& prompt) {
     return input;
 }
 
+
 int VehicleConfiguration::inputQuantity() {
     std::string input;
     int quantity;
@@ -26,62 +27,74 @@ int VehicleConfiguration::inputQuantity() {
     return quantity;
 }
 
-std::string VehicleConfiguration::setColor(std::string prompt) {
-    char choice = toupper(prompt[0]);
-    switch (choice) {
-      case 'W': return "White";
-      case 'G': return "Grey";
-      case 'B': return "Black";
-      case 'Q': throw UserQuitException();
-      default: throw std::runtime_error("Error reading color");
+std::string VehicleConfiguration::getOptionFromUser(
+    const std::string& prompt, 
+    const std::unordered_map<char, std::string>& options) {
+  
+    while (true) {
+        char choice = toupper(inputWithPrompt(prompt)[0]);
+        
+        // Check if user wants to quit.
+        if (choice == 'Q') {
+            throw UserQuitException();
+        }
+
+        // Check if choice is valid.
+        auto found = options.find(choice);
+        if (found != options.end()) {
+            return found->second;
+        }
+
+        std::cout << "Invalid option. Please try again." << std::endl;
     }
+}
+
+std::string VehicleConfiguration::setColor() {
+  return getOptionFromUser(
+      "Color (W)hite, (G)ray, (B)lack: ",
+      {
+          {'W', "White"},
+          {'G', "Gray"},
+          {'B', "Black"},
+      });
 }
 
 std::string VehicleConfiguration::setEngineType() {
-    do {
-      auto type = inputWithPrompt("EV or IC: ");
-      type = toupper(type[0]);
-      if (type == "E" || type == "EV") return "EV";
-      if (type == "I" || type == "IC") return "IC";
-      if (type == "Q") throw UserQuitException();
-    } while (true);
+  return getOptionFromUser(
+      "Engine Type (EV) or (IC): ",
+      {
+          {'E', "EV"},
+          {'I', "IC"},
+      });
 }
 
 std::string VehicleConfiguration::setCargoOrPassenger() {
-  while (true) {
-    char choice = toupper(inputWithPrompt("(C)argo or (P)assenger: ")[0]);
-    switch (choice) {
-        case 'C': return "Cargo";
-        case 'P': return "Passenger";
-        case 'Q': throw UserQuitException();
-        default: throw std::runtime_error("Error reading cargo or passenger type");
-    }
-  }
+  return getOptionFromUser(
+      "Cargo or Passenger (C)argo or (P)assenger: ",
+      {
+          {'C', "Cargo"},
+          {'P', "Passenger"},
+      });
 }
+
 std::string VehicleConfiguration::setCargoRoofline() {
-  while (true) {
-    char choice = toupper(inputWithPrompt("Cargo Roofline (L)ow, (R)aised, (H)igh: ")[0]);
-    switch (choice) {
-        case 'L': return "Low";
-        case 'R': return "Raised";
-        case 'H': return "High";
-        case 'Q': throw UserQuitException();
-        default: throw std::runtime_error("Error reading cargo roofline");
-    }
-  }
+  return getOptionFromUser(
+      "Cargo Roofline (S)hort, (M)edium, (T)all: ",
+      {
+          {'S', "Short"},
+          {'M', "Medium"},
+          {'T', "Tall"},
+      });
 }
 
 std::string VehicleConfiguration::setWheelbase() {
-  while (true) {
-    char choice = toupper(inputWithPrompt("Wheelbase (S)hort, (M)edium, (E)xtended: ")[0]);
-    switch (choice) {
-        case 'S': return "Short";
-        case 'M': return "Medium";
-        case 'E': return "Extended";
-        case 'Q': throw UserQuitException();
-        default: throw std::runtime_error("Error reading wheelbase");
-    }
-  }
+  return getOptionFromUser(
+      "Wheelbase (S)hort, (M)edium, (L)ong: ",
+      {
+          {'S', "Short"},
+          {'M', "Medium"},
+          {'L', "Long"},
+      });
 }
 
 // we need to collect the data pass it to our database class to be stored in a database
@@ -90,7 +103,7 @@ void VehicleConfiguration::collectData() {
     carData.dealerName = inputWithPrompt("Dealer Name: ");
     carData.memoReference = inputWithPrompt("Memo Reference: ");
     carData.quantity = inputQuantity();
-    carData.color = setColor(inputWithPrompt("Color (W)hite, (G)ray, (B)lack: "));
+    carData.color = setColor();
     carData.evOrIc = setEngineType();
     setCarData(carData.quantity);
     if (carData.evOrIc != "EV") {
--- VehicleConfiguration.hpp
+++ VehicleConfiguration.hpp
@@ -9,38 +9,37 @@
 #include <cctype>
 #include <limits>
 #include <fstream>
-#include <cctype>
-#include <cstdlib>
+#include <unordered_map>  // Added header for unordered_map
+#include <functional>     // Added header for std::function
 
-// Declaration of the 'car' struct
 struct car {
     float carID;
     std::string dealerName, memoReference, color, evOrIc, cargoOrPassenger, cargoRoofline, wheelbase;
     int year, quantity;
 };
 
-// Declaration of the 'VehicleConfiguration' class
 class VehicleConfiguration {
 public:
-  using Callback = std::function<void()>;
-  void setMenuCallback(Callback callback) { menuCallback = std::move(callback); }
-  void run();
-  void collectData();
-  car getCarData() const { return carData; }
+    using Callback = std::function<void()>;
+    void setMenuCallback(Callback callback) { menuCallback = std::move(callback); }
+    void run();
+    void collectData();
+    car getCarData() const { return carData; }
 
 private:
-  car carData;
-
-  Callback menuCallback;
-  void welcomeMessage();
-  std::string inputWithPrompt(const std::string& prompt);
-  int inputQuantity();
-  std::string setColor(std::string prompt);
-  std::string setEngineType();
-  std::string setCargoOrPassenger();
-  std::string setCargoRoofline();
-  std::string setWheelbase();
-  void setCarData(int quantity);
+    car carData;
+    Callback menuCallback;
+    void welcomeMessage();
+    std::string inputWithPrompt(const std::string& prompt);
+    int inputQuantity();
+    std::string setColor();  // Removed parameter
+    std::string setEngineType();
+    std::string setCargoOrPassenger();
+    std::string setCargoRoofline();
+    std::string setWheelbase();
+    // Added the following method declaration
+    std::string getOptionFromUser(const std::string& prompt, const std::unordered_map<char, std::string>& options);
+    void setCarData(int quantity);
 };
 
 #endif // VEHICLECONFIGURATION_HPP
