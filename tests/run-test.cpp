a81b6e611c7fedde8b491110c3b0dedcd3080ab2
brendanglancy <bglance68@gmail.com>
2023-10-16 01:56:44 -0400

    the test now passes as it should

--- tests/run-test.cpp
+++ tests/run-test.cpp
@@ -9,6 +9,7 @@ void testDatabaseConstructor() {
 
   // Test 1: Ensure the database file is created.
   assert(std::filesystem::exists("./docs/database.db"));
+  db.seedDatabase();
 
   // Test 2 and 3 can involve querying the database to ensure the table exists
   // and has the expected structure. For now, just illustrating the setup.
